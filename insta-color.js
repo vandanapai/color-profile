0472b79398e2f19700a00624e8ae962449b6c16a
Vandana Pai <vandanapai01@gmail.com>
2014-10-18 15:51:29 -0700

    added separate js file

---
+++ insta-color.js
@@ -0,0 +1,35 @@
+$( window ).load(function() {
+
+	$('#submit-image').click(function() {
+		var imgURL = $('#user-input').val();
+		console.log(imgURL);
+
+
+		$('<img src="' + imgURL + '">').load(function() {
+			var colorThief = new ColorThief();
+
+			console.log(this); // check image URL
+
+			var domColor = colorThief.getColor(this);
+			$('#img-color').css('background-color', 'rgb(' + domColor[0] + ',' + domColor[1] + ',' + domColor[2] + ')');  // display the dominant color
+			$('.img-container').css('background-color', 'rgb(' + domColor[0] + ',' + domColor[1] + ',' + domColor[2] + ')'); // load dom color bg for modal
+
+			// load the image in a new div hidden
+
+			$('#loaded-img').prepend(this).css;
+
+			// on click display the image in a modal
+
+			$('#img-color').click(function() {
+				$('.img-container').fadeIn(200);
+				    console.log("showing");
+				});
+			$('#img-close').click(function() {
+				$('.img-container').fadeOut(200);
+				});
+
+
+
+			});
+		});
+	});
